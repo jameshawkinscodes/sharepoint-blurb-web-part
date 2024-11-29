a9a55ba2b7c627330ac9b0ae1e48e861bafcb0c2
jhawkins <jhawkins@snolab.ca>
2024-11-29 12:28:00 -0500

    Bug fix and bumped version to 0.1.0

--- src/webparts/blurb/Util/Util.ts
+++ src/webparts/blurb/Util/Util.ts
@@ -1,31 +1,48 @@
 // Utility file: src/utils/Util.ts
 
+interface SortableItem {
+  SortWeight: number;
+}
+
 export class Util {
-    // Function to generate a unique ID
-    public static GenerateId(): string {
-      return 'id-' + Math.random().toString(36).substr(2, 16);
-    }
-  
-    // Function to calculate the sort weight for blurbs when they are reordered
-    public static CalculateNewSortWeight(items: any[], newIndex: number, oldIndex?: number): number {
-      const sortedItems = [...items].sort((a, b) => a.SortWeight - b.SortWeight);
-  
-      let newSortWeight = 0;
-  
-      if (newIndex === 0) {
-        // If the new index is at the start, calculate a sort weight less than the first item
-        newSortWeight = sortedItems[0].SortWeight - 1;
-      } else if (newIndex === sortedItems.length - 1) {
-        // If the new index is at the end, calculate a sort weight greater than the last item
-        newSortWeight = sortedItems[sortedItems.length - 1].SortWeight + 1;
-      } else {
-        // Otherwise, calculate a sort weight between the surrounding items
-        const prevItem = sortedItems[newIndex - 1];
-        const nextItem = sortedItems[newIndex];
-        newSortWeight = (prevItem.SortWeight + nextItem.SortWeight) / 2;
-      }
-  
-      return newSortWeight;
-    }
+  /**
+   * Generates a unique ID.
+   * @returns A unique string ID.
+   */
+  public static GenerateId(): string {
+    return 'id-' + Math.random().toString(36).substr(2, 16);
   }
-  
+
+  /**
+   * Calculates the new sort weight for an item when reordered.
+   * @param items - The array of sortable items.
+   * @param newIndex - The new index of the item being reordered.
+   * @param oldIndex - The original index of the item (optional).
+   * @returns The new sort weight for the item.
+   */
+  public static CalculateNewSortWeight(
+    items: SortableItem[],
+    newIndex: number,
+    oldIndex?: number
+  ): number {
+    // Sort items by their SortWeight property
+    const sortedItems = [...items].sort((a, b) => a.SortWeight - b.SortWeight);
+
+    let newSortWeight = 0;
+
+    if (newIndex === 0) {
+      // If the new index is at the start, calculate a sort weight less than the first item
+      newSortWeight = sortedItems[0].SortWeight - 1;
+    } else if (newIndex === sortedItems.length) {
+      // If the new index is at the end, calculate a sort weight greater than the last item
+      newSortWeight = sortedItems[sortedItems.length - 1].SortWeight + 1;
+    } else {
+      // Otherwise, calculate a sort weight between the surrounding items
+      const prevItem = sortedItems[newIndex - 1];
+      const nextItem = sortedItems[newIndex];
+      newSortWeight = (prevItem.SortWeight + nextItem.SortWeight) / 2;
+    }
+
+    return newSortWeight;
+  }
+}
